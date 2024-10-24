7c6e6f16d95200c5e232eda246aca39088aa98c3
dylanklark <62446463+dylanklark@users.noreply.github.com>
2024-10-23 23:41:04 -0400

    Same thing

--- src/WTCValenzuelaClark.java
+++ src/WTCValenzuelaClark.java
@@ -8,7 +8,11 @@ public class WTCValenzuelaClark {
         Scanner input = new Scanner(System.in);
         System.out.print("You can choose a type of transportation you prefer.\nPlease enter either" +
                         " Air, Water, or Land: ");
-        input.nextLine();
+        String transportType = input.nextLine();
+        if (transportType.toUpperCase() == "AIR") {
+            String format = String.format("%s\t%s\t%s\t%s\t%s%n");
+            System.out.printf();
+        }
 
     }
 }
