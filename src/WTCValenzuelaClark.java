2f122623872da77e5dd91e34e8c7264f306bbd87
dylanklark <62446463+dylanklark@users.noreply.github.com>
2024-10-24 03:38:39 -0400

    Same thing

--- src/WTCValenzuelaClark.java
+++ src/WTCValenzuelaClark.java
@@ -11,41 +11,59 @@ public class WTCValenzuelaClark {
         String transportType = input.nextLine();
 
         if (transportType.toUpperCase().equals("AIR")) {
+            Transportation transportOption = null;
             while (true) {
                 Plane planeTicket = new Plane(100, 550, "Ticket", 160);
                 Helicopter helicopterTicket = new Helicopter(500, 125, "Ticket", 6);
                 Dirigible dirgibleTicket = new Dirigible(500, 35, "Ticket", 15);
                 HotAirBalloon airBalloonTicket = new HotAirBalloon(250, 10, "Ticket", 10);
-                System.out.print("\nYour air transportation options are: \n" + "-".repeat(70) + "\n");
-                String format = String.format("%%%ds\t%%%ds\t%%%ds\t%%%ds\t%%%ds%n", -15, -5, -5, -5, -5);
-                System.out.printf(format, "Option", "Cost", "Average Speed", "Rental/Ticket", "Number of Passengers");
-                System.out.print(planeTicket.tableFormat());
-                System.out.print(helicopterTicket.tableFormat());
-                System.out.print(dirgibleTicket.tableFormat());
-                System.out.print(airBalloonTicket.tableFormat());
-
-                System.out.print("\nWhich transportation option would you like to use? ");
-                String transportOption = input.nextLine();
-                switch (transportOption.toUpperCase()) {
-                    case "PLANE":
-                        System.out.print(planeTicket);
-                        break;
-                    case "HELICOPTER":
-                        System.out.print(helicopterTicket);
-                        break;
-                    case "DIRIGIBLE":
-                        System.out.print(dirgibleTicket);
-                        break;
-                    case "HOT AIR BALLOON":
-                        System.out.print(airBalloonTicket);
-                        break;
-                }
-                System.out.printf("%nYou selected %s%n, " +
-                        "Enter 'yes' to continue or 'no' to change your preferred transportation method.", transportOption);
+                displayTable(planeTicket, helicopterTicket, dirgibleTicket, airBalloonTicket);
+                transportOption = selectOption(planeTicket, helicopterTicket, dirgibleTicket, airBalloonTicket);
+                System.out.printf("%n%nYou selected '%s'%n" +
+                        "Enter 'yes' to continue or 'no' to change your preferred transportation method. ",
+                        transportOption.getTransportationName());
                 String userChoice = input.nextLine().trim();
-                if ("yes".equals(userChoice.toLowerCase())) {continue;}
-                else {break;}
+                if (userChoice.toLowerCase().equals("yes")) {break;}
+                else {continue;}
             }
+            System.out.print("How many passengers will be travelling with you? ");
+            int passengerNum = input.nextInt();
+            int totalCost = (int) (passengerNum * transportOption.getCost());
+            System.out.printf("Your total cost is: %d dollars.", totalCost);
+        }
+    }
+
+    public static void displayTable(Plane planeTicket, Helicopter helicopterTicket, Dirigible dirgibleTicket,
+                                       HotAirBalloon airBalloonTicket) {
+        System.out.print("\nYour air transportation options are: \n" + "-".repeat(70) + "\n");
+        String format = String.format("%%%ds\t%%%ds\t%%%ds\t%%%ds\t%%%ds%n", -15, -5, -5, -5, -5);
+        System.out.printf(format, "Option", "Cost", "Average Speed", "Rental/Ticket", "Number of Passengers");
+        System.out.print(planeTicket.tableFormat());
+        System.out.print(helicopterTicket.tableFormat());
+        System.out.print(dirgibleTicket.tableFormat());
+        System.out.print(airBalloonTicket.tableFormat());
+    }
+
+    public static Transportation selectOption(Plane planeTicket, Helicopter helicopterTicket, Dirigible dirgibleTicket,
+                                      HotAirBalloon airBalloonTicket) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("\nWhich transportation option would you like to use? ");
+        String transportOption = input.nextLine();
+        switch (transportOption.toUpperCase()) {
+            case "PLANE":
+                System.out.print(planeTicket);
+                return planeTicket;
+            case "HELICOPTER":
+                System.out.print(helicopterTicket);
+                return helicopterTicket;
+            case "DIRIGIBLE":
+                System.out.print(dirgibleTicket);
+                return dirgibleTicket;
+            case "HOT AIR BALLOON":
+                System.out.print(airBalloonTicket);
+                return  airBalloonTicket;
+            default:
+                return null;
         }
     }
 }
