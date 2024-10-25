2ed04691430b4dd3faf4b4300d995f7198f57377
Rosebud2003 <140856971+Rosebud2003@users.noreply.github.com>
2024-10-25 07:58:12 -0400

    Update Bus.java

--- src/Bus.java
+++ src/Bus.java
@@ -1,7 +1,21 @@
-public class Bus {
-    public static class Automobile {
-        //another test
-        //test
-        // another another test
+import java.util.Date;
+
+public class Bus extends Automobile {
+
+    public Bus(double cost, double averageSpeed,
+                         String purchaseType, int numPassengers, Date reservationTime) {
+        super("Bus", cost, averageSpeed, purchaseType, numPassengers, reservationTime);
+    }
+
+    public Bus(double cost, double averageSpeed, String purchaseType, int numPassengers) {
+        super("Bus", cost, averageSpeed, purchaseType, numPassengers);    }
+
+    public String tableFormat() {
+        return super.tableFormat();
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
     }
 }
