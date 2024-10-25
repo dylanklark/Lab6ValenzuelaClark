b90f5a1883bddd70a9c8be661a3d2c31621d4adf
Rosebud2003 <140856971+Rosebud2003@users.noreply.github.com>
2024-10-25 07:51:36 -0400

    Update Automobile.java

--- src/Automobile.java
+++ src/Automobile.java
@@ -1,2 +1,26 @@
-public class Automobile {
+import java.util.Date;
+
+public class Automobile extends LandTransportation {
+
+    public Automobile(String Bus, double cost, double averageSpeed,
+                     String purchaseType, int numPassengers, Date reservationTime) {
+        super("Automobile", cost, averageSpeed, purchaseType, numPassengers, reservationTime);
+    }
+
+    public Automobile(int cost, int averageSpeed, String purchaseType, int numPassengers) {
+        super("Automobile", cost, averageSpeed, purchaseType, numPassengers);
+    }
+
+    public Automobile(String hotAirBaloon, int cost, int averageSpeed, String purchaseType, int numPassengers) {
+        super("Automobile", cost, averageSpeed, purchaseType, numPassengers);
+    }
+
+    public String tableFormat() {
+        return super.tableFormat();
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
 }
