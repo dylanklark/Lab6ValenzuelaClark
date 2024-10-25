8a6c971822f9d07c00bdbe2a0ce1e25741044853
Rosebud2003 <140856971+Rosebud2003@users.noreply.github.com>
2024-10-25 07:53:12 -0400

    Update WaterTransportation.java

--- src/WaterTransportation.java
+++ src/WaterTransportation.java
@@ -1,1 +1,16 @@
+import java.util.Date;
+public class WaterTransportation extends Transportation {
+    public WaterTransportation(String transportationName, double cost, double averageSpeed,
+                              String purchaseType, int numPassengers, Date reservationTime){
+        super("Water", transportationName, cost, averageSpeed, purchaseType,
+                numPassengers, reservationTime);
+    }
+    public WaterTransportation(String transportationName, double cost, double averageSpeed,
+                              String purchaseType, int numPassengers){
+        super("Water", transportationName, cost, averageSpeed, purchaseType, numPassengers);
+    }
+    public String tableFormat() {return super.tableFormat();}
+    @Override
+    public String toString() {return super.toString();}
+}
 
