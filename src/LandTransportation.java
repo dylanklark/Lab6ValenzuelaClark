ab6a47da159d97119ee075c606b5314a957d8bf3
Rosebud2003 <140856971+Rosebud2003@users.noreply.github.com>
2024-10-25 07:46:18 -0400

    Update LandTransportation.java

--- src/LandTransportation.java
+++ src/LandTransportation.java
@@ -1,2 +1,15 @@
-public class LandTransportation {
+import java.util.Date;
+public class LandTransportation extends Transportation {
+    public LandTransportation(String transportationName, double cost, double averageSpeed,
+                              String purchaseType, int numPassengers, Date reservationTime){
+        super("Land", transportationName, cost, averageSpeed, purchaseType,
+                numPassengers, reservationTime);
+    }
+    public LandTransportation(String transportationName, double cost, double averageSpeed,
+                              String purchaseType, int numPassengers){
+        super("Land", transportationName, cost, averageSpeed, purchaseType, numPassengers);
+    }
+    public String tableFormat() {return super.tableFormat();}
+    @Override
+    public String toString() {return super.toString();}
 }
