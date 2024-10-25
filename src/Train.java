f6fb7e74cbce0fa0c7db15aef8afe1c9c56af5a1
Rosebud2003 <140856971+Rosebud2003@users.noreply.github.com>
2024-10-25 07:46:47 -0400

    Update Train.java

--- src/Train.java
+++ src/Train.java
@@ -1,2 +1,13 @@
-public class Train {
+import java.util.Date;
+public class Train extends LandTransportation {
+    public Train(double cost, double averageSpeed, String purchaseType,
+                 int numPassengers, Date reservationTime){
+        super("Train", cost, averageSpeed, purchaseType, numPassengers, reservationTime);
+    }
+    public Train(double cost, double averageSpeed, String purchaseType, int numPassengers){
+        super("Train", cost, averageSpeed, purchaseType, numPassengers);
+    }
+    public String tableFormat() {return super.tableFormat();}
+    @Override
+    public String toString() {return super.toString();}
 }
