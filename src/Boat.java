efea4251a58f163da0d5f66ae999c7ff4c13b48d
Rosebud2003 <140856971+Rosebud2003@users.noreply.github.com>
2024-10-25 12:25:46 -0400

    Update Boat.java

--- src/Boat.java
+++ src/Boat.java
@@ -1,5 +1,14 @@
-public class Boat {
-    public static class AirTransportation {
-        // test
+import java.util.Date;
+
+public class Boat extends WaterTransportation {
+    public Boat(double cost, double averageSpeed, String purchaseType,
+                int numPassengers, Date reservationTime){
+        super("Boat", cost, averageSpeed, purchaseType, numPassengers, reservationTime);
     }
+    public Boat(double cost, double averageSpeed, String purchaseType, int numPassengers){
+        super("Boat", cost, averageSpeed, purchaseType, numPassengers);
+    }
+    public String tableFormat() {return super.tableFormat();}
+    @Override
+    public String toString() {return super.toString();}
 }
